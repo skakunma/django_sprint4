dd9b5ad4a4b2de4f0fed3a91cf6858bb4db8fea7
Дмитрий Скакун <skakun2025@mail.ru>
2024-05-27 00:59:55 +0300

    sdkjf

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -18,9 +18,11 @@ class LocationAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'category', 'location', 'pub_date', 'is_published', 'created_at']
+    list_display = ['title', 'author', 'category', 'location',
+                    'pub_date', 'is_published', 'created_at']
     search_fields = ['title', 'text', 'author__username']
-    list_filter = ['pub_date', 'author', 'category', 'location', 'is_published', 'created_at']
+    list_filter = ['pub_date', 'author', 'category',
+                   'location', 'is_published', 'created_at']
 
 
 @admin.register(Comment)
